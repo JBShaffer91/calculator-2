780c376d23907893ae1acd85cadf6772e2f24207
Justin Shaffer <justinbshaffer91@gmail.com>
2023-01-18 09:07:07 -0800

    separate business and UI logic, create user prompts to collect two numbers, return result of the addition of the two numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
-function saySomething(whatToSay) {
-  window.alert(whatToSay);
-}
-
-saySomething("hi");
+//Business Logic //
 
 function add(number1, number2) {
   return number1 + number2;
 }
 
-const result = add(3, 5);
-window.alert(result);
+// User Interface Logic //
 
+function saySomething(whatToSay) {
+  window.alert(whatToSay);
+}
+
+saySomething("Hello there!");
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+
+const outputText = "The sum is: " + add(number1, number2);
+saySomething(outputText);
