09b1bc0fdfd5d00ae5cb894477a91de2f9936693
Justin Shaffer <justinbshaffer91@gmail.com>
2023-01-18 09:20:03 -0800

    change outputText to let, reassign outputText to show result of each calculation

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,18 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
 // User Interface Logic //
 
 function saySomething(whatToSay) {
@@ -15,5 +27,14 @@ saySomething("Hello there!");
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-const outputText = "The sum is: " + add(number1, number2);
+let outputText = "The sum is: " + add(number1, number2);
+saySomething(outputText);
+
+outputText = "The difference is: " + subtract(number1, number2);
+saySomething(outputText);
+
+outputText = "The product is: " + multiply(number1, number2);
+saySomething(outputText);
+
+outputText = "The quotient is: " + divide(number1, number2);
 saySomething(outputText);
