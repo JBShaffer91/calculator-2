8ca8b988d6616aab008a4a3759ea2221c7eaadd3
Justin Shaffer <justinbshaffer91@gmail.com>
2023-01-18 07:50:49 -0800

    add paths for css and js to the DOM, create a greeting and addition window alerts

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function saySomething(whatToSay) {
+  window.alert(whatToSay);
+}
+
+saySomething("hi");
+
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+const result = add(3, 5);
+window.alert(result);
+
